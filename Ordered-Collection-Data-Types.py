712f16d70063a2f0e527cff2e03cbb150423a5f8
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2021-02-11 08:39:38 -0700

    implement stack

---
+++ Ordered-Collection-Data-Types.py
@@ -0,0 +1,23 @@
+#Jacob Hardman
+#CS301 Algorithms and Data Structures
+#Dr. Nathaniel Miller
+
+#Stack
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
