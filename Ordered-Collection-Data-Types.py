4eb33524f6d428ce0a0dd33241a469978d6992c3
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2021-02-11 08:51:53 -0700

    implement queue

--- Ordered-Collection-Data-Types.py
+++ Ordered-Collection-Data-Types.py
@@ -21,3 +21,19 @@ class Stack:
 
     def size(self):
         return len(self.items)
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
